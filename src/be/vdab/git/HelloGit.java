e094040a36dd752408ddceb189b391dd2b5e86ea
Wietse Mees <wietsemees@hotmail.com>
2023-10-06 13:05:26 +0200

    Bon appetit commit

--- src/be/vdab/git/HelloGit.java
+++ src/be/vdab/git/HelloGit.java
@@ -3,6 +3,6 @@ package be.vdab.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Bon appetit!");
+        System.out.println("Bon appetit !");
     }
 }
